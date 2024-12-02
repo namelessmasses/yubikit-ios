825bce5d4d7c65c20c33b5cd80d52d8b628d2de6
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2024-12-02 07:04:40 +0000

    Add YKFOpenPGPHashAlgorithm enumeration for hash algorithms

---
+++ YubiKit/YubiKit/Connections/Shared/APDU/OpenPGP/YKFOpenPGPHashAlgorithm.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, YKFOpenPGPHashAlgorithm) {
+  YKFOpenPGPHashAlgorithmSHA256,
+  YKFOpenPGPHashAlgorithmSHA384,
+  YKFOpenPGPHashAlgorithmSHA512,
+  YKFOpenPGPHashAlgorithmECDSA
+};
