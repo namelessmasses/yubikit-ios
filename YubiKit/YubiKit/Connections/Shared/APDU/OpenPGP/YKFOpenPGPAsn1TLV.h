190eb431feef425045cf1b91c96ca07cded5d437
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2024-12-03 04:53:57 +0000

    APDU/OpenPGP: implement ASN.1 ISO-7816 Part 4 TLVs

--- YubiKit/YubiKit/Connections/Shared/APDU/OpenPGP/YKFOpenPGPAsn1TLV.h
+++ YubiKit/YubiKit/Connections/Shared/APDU/OpenPGP/YKFOpenPGPAsn1TLV.h
@@ -1,6 +1,310 @@
+#include "YKFOpenPGPHashAlgorithm.h"
+#import <Foundation/Foundation.h>
+
+/**
+ * @struct YKFOpenPGPTLV
+ * @brief A structure representing a TLV (Tag-Length-Value) element used in
+ * OpenPGP.
+ *
+ * This structure is used to represent a TLV element where:
+ * - 1-byte `tag`
+ * - 1-byte `length` simple length. Does not support ASN.1 endcoded length.
+ * - `value` is a flexible array member that holds the actual data.
+ *
+ * The structure is packed to ensure there is no padding between the fields.
+ *
+ * @note Not related to ASN.1 SIMPLE-TLV.
+ */
 typedef struct __attribute__((__packed__)) {
-  uint8_t tag;
-  uint8_t length;
-  uint8_t value[0];
+  UInt8 tag;
+  UInt8 length;
+  UInt8 value[0];
 } YKFOpenPGPTLV;
 
+/**
+ * @brief Tag used commonly in OpenPGP commands and responses.
+ *
+ * Provides an abstraction across SIMPLE-TLV and BER-TLV tags.
+ */
+@protocol YKFOpenPGPTLVTagProtocol
+
+/**
+ * @brief Bytes of an encoded tag. Allows multi-byte tags.
+ */
+@property(nonatomic, readonly) NSData *encodedBytes;
+
+/**
+ * @brief Tag number from a decoded tag
+ */
+@property(nonatomic, readonly) UInt64 tagNumber;
+
+/**
+ * @brief Extract the byte of the tag when passing P1 in a command APDU.
+ */
+@property(nonatomic, readonly) UInt8 p1;
+
+/**
+ * @brief Extract te byte of the tage when passing P2 in a command APDU.
+ */
+@property(nonatomic, readonly) UInt8 p2;
+
+/**
+ * @brief Get the tag as command parameters P1 and P2 simultaneously.
+ *
+ * *p1 and *p2 will be unchanged under the following error conditions:
+ * - The tag is longer than 2 bytes.
+ * - The both of the out parameters are nil.
+ *
+ * @param p1 out-param of p1
+ * @param p2 out-param of p2
+ */
+- (void)getParametersP1:(UInt8 *)p1 P2:(UInt8 *)p2;
+
+@end
+
+/**
+ * @brief Tag with a single byte value.
+ */
+@interface YKFOpenPGPSimpleTLVTag : NSObject <YKFOpenPGPTLVTagProtocol>
+
+/**
+ * @brief Parses the encoded bytes to ensure they correctly encode a SIMPLE-TLV
+ * tag.
+ */
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes;
+
+/**
+ * @brief Initialize with the given tag value.
+ */
+- (instancetype)initWithValue:(UInt8)value;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+/**
+ * @brief Tag encoded with ASN.1 Basic Encoding Rules (BER) formt.
+ *
+ * @see ISO-7816 Part 4 Annex D.
+ *
+ * Leading byte:
+ * - B8 B7: class
+ *   - 00: universal
+ *   - 01: application
+ *   - 10: context-specific
+ *   - 11: private
+ * - B6: type
+ *   - 0: primitive
+ *   - 1: constructed
+ * - B5-B1:
+ *   - 0b00000 - 0b11110: tag number
+ *   - 0b11111: tag number continues on one or more subsequent bytes.
+ *
+ * Subequent bytes:
+ * - B8 of each subsequent byte: 1, unless last byte.
+ * - B7-B1 of the first subsequent byte shall not be 0.
+ * - B7-B1 of each subsequent byte, including the last, encode an integer
+ *   equal to the tag number.
+ */
+@interface YKFOpenPGPBERTLVTag : NSObject <YKFOpenPGPTLVTagProtocol>
+
+typedef NS_ENUM(NSUInteger, YKFOpenPGPBERTLVTagClass) {
+  YKFOpenPGPBERTLVTagClassUniversal = 0x00,
+  YKFOpenPGPBERTLVTagClassApplication = 0x01,
+  YKFOpenPGPBERTLVTagClassContextSpecific = 0x02,
+  YKFOpenPGPBERTLVTagClassPrivate = 0x03
+} NS_SWIFT_NAME(YKFOpenPGPBERTLVTag.Class);
+
+typedef NS_ENUM(NSUInteger, YKFOpenPGPBERTLVTagType) {
+  YKFOpenPGPBERTLVTagTypePrimitive = 0x00,
+  YKFOpenPGPBERTLVTagTypeConstructed = 0x01
+} NS_SWIFT_NAME(YKFOpenPGPBERTLVTag.Type);
+
+/**
+ * @brief Initialize with the given encoded bytes.
+ *
+ * @param encodedBytes parsed to check for correctness according to the BER
+ * format.
+ */
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes;
+
+/**
+ * @brief Initialize and encode the given class, type, and tag number.
+ */
+- (instancetype)initWithClass:(YKFOpenPGPBERTLVTagClass)class
+                         type:(YKFOpenPGPBERTLVTagType)type
+                    tagNumber:(UInt64)tagNumber;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+/**
+ * @brief Encoded length. Handles values up to 65535.
+ */
+@protocol YKFOpenPGPTLVLengthProtocol
+
+/**
+ * @brief Encoded bytes
+ */
+@property(nonatomic, readonly) NSData *encodedBytes;
+
+/**
+ * @brief length as a simple integer value.
+ */
+@property(nonatomic, readonly) uint16_t integerValue;
+
+@end
+
+/**
+ * @brief Length encoded to ISO-7816 Part 4 section 5.4.4.
+ */
+@interface YKFOpenPGPSimpleTLVLength : NSObject <YKFOpenPGPTLVLengthProtocol>
+
+/**
+ * @brief Initialize with pre-encoded bytes.
+ *
+ * Encoded bytes are checked for correctness according to the ISO-7816 Part 4
+ * section 5.4.4.
+ *
+ * @param encodedBytes If the leading byte is the range ['0x00', '0xFE'], then
+ * the length field is a single byte encoding an integer L valued from 0 to 254.
+ * If the leading byte is `FF`, then the length field continues on the two
+ * subsequent bytes which encodes a big-endian integer, L, with a value of 0 to
+ * 65535.
+ */
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes;
+
+/**
+ * @brief Encodes the integer according to ISO-7816 Part 4 section 5.4.4.
+ *
+ * @param i encodes the integer into 1 or 3 bytes. 0 <= i < 255 encodes as `i`.
+ * 255 <= i < 65536 encodes as `FF ii ii`.
+ */
+- (instancetype)initWithInteger:(uint16_t)length;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+/**
+ * @brief Length encoded to ISO-7816 Part 4 Annex D.
+ */
+@interface YKFOpenPGPBERTLVLength : NSObject <YKFOpenPGPTLVLengthProtocol>
+
+typedef NS_ENUM(NSUInteger, YKFOpenPGPBERTLVLengthForm) {
+  YKFOpenPGPBERTLVLengthFormShort,
+  YKFOpenPGPBERTLVLengthFormLong,
+} NS_SWIFT_NAME(YKFOpenPGPBERTLVLength.Form);
+
+/**
+ * @brief Initialize with pre-encoded bytes.
+ *
+ * Encoded bytes are checked for correctness according to the ISO-7816 Part 4
+ * Annex D.
+ *
+ * @param encodedBytes If the leading byte is in the range ['0x00', '0x7F'],
+ * then the length field is a single byte encoding an integer L valued from 0 to
+ * 127. If the leading byte is `0x81`, then the length field continues on the
+ * following byte which encodes an integer L valued from 0 to 255. If the
+ * leading byte is `0x82`, then the length field continues on the two subsequent
+ * bytes which encodes a big-endian integer, L, with a value of 0 to 65535.
+ */
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes;
+
+/**
+ * @brief Initialize with a simple integer length.
+ *
+ * @param length If length is in the range [0, 255], then the length field is a
+ * single byte encoding an integer L valued from 0 to 127. If length is in the
+ * range [256, 65535], then the length field is three bytes encoding an integer
+ * L valued from 0 to 65535.
+ *
+ * @param form attempts to use this form to encode the length.
+ *
+ * @return nil if the length is out of range for the form.
+ */
+- (instancetype)initWithInteger:(UInt16)length
+                           form:(YKFOpenPGPBERTLVLengthForm)form;
+
+@end
+
+/**
+ * @brief A common abstraction used across `SIMPLE-TLV` and `BER-TLV`.
+ */
+@protocol YKFOpenPGPTLVProtocol <NSObject>
+
+@property(nonatomic, readonly) NSData *encodedBytes;
+
+@property(nonatomic, readonly) id<YKFOpenPGPTLVTagProtocol> tag;
+
+@property(nonatomic, readonly) id<YKFOpenPGPTLVLengthProtocol> length;
+
+/**
+ * @brief value.length == length.asInteger
+ */
+@property(nonatomic, readonly) NSData *value;
+
+@end
+
+/**
+ * @brief Representation of `SIMPLE-TLV`.
+ *
+ * Enforces 1-byte tag.
+ */
+@interface YKFOpenPGPSimpleTLV : NSObject <YKFOpenPGPTLVProtocol>
+
+/**
+ * @brief Initialize with pre-encoded bytes.
+ *
+ * @param encodedBytes Parse for the tag, length, and value. Each checked for
+ * correctness.
+ */
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes;
+
+/**
+ * @brief Initialize with existing tag, length, and value.
+ *
+ * Encodes the tag, length, and value into `encodedBytes`.
+ *
+ * @param value checked for length matching `length.integerValue`.
+ */
+- (instancetype)initWithTag:(YKFOpenPGPSimpleTLVTag *)tag
+                     length:(YKFOpenPGPSimpleTLVLength *)length
+                      value:(NSData *)value;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+#if 0
+
+/**
+ * @brief Representation of `BER-TLV`.
+ *
+ * Allows multi-byte tags.
+ */
+@interface YKFOpenPGPBERTLV : NSObject <YKFOpenPGPTLVProtocol>
+
+/**
+ * @brief Initialize with pre-encoded bytes.
+ *
+ * @param encodedBytes Parse tag, length, and value. Each checked for
+ * correctness.
+ */
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes;
+
+/**
+ * @brief Initialize with existing tag, length, and value.
+ *
+ * @param tag Allows multi-byte tags.
+ * @param length Parses  */
+- (instancetype)initWithTag:(YKFOpenPGPBEGTag *)tag
+                     length:(YKFOpenPGPBERLength *)length
+                      value:(NSData *)value;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
+
+#endif
---
+++ YubiKit/YubiKit/Connections/Shared/APDU/OpenPGP/YKFOpenPGPAsn1TLV.m
@@ -0,0 +1,447 @@
+#import "YKFOpenPGPAsn1TLV.h"
+
+#import <Foundation/Foundation.h>
+
+@interface YKFOpenPGPSimpleTLVTag ()
+
+@property(nonatomic, readwrite) NSData *encodedBytes;
+
+@end
+
+@implementation YKFOpenPGPSimpleTLVTag
+
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes {
+  self = [super init];
+  if (self == nil) {
+    return nil;
+  }
+
+  if (encodedBytes.length < 1) {
+    return nil;
+  }
+
+  self.encodedBytes = [encodedBytes subdataWithRange:NSMakeRange(0, 1)];
+
+  return self;
+}
+
+- (instancetype)initWithValue:(UInt8)value {
+  self = [super init];
+  if (self) {
+    self.encodedBytes = [NSData dataWithBytes:&value length:1];
+  }
+  return self;
+}
+
+- (UInt64)tagNumber {
+  UInt8 v;
+  [self.encodedBytes getBytes:&v length:sizeof(v)];
+  return v;
+}
+
+- (UInt8)p1 {
+  return 0x00;
+}
+
+- (UInt8)p2 {
+  return self.tagNumber;
+}
+
+- (void)getParametersP1:(UInt8 *)p1 P2:(UInt8 *)p2 {
+  if (p1 == nil && p2 == nil) {
+    return;
+  }
+
+  *p1 = self.p1;
+  *p2 = self.p2;
+}
+
+- (NSString *)description {
+  return [NSString stringWithFormat:@"<SIMPLE-TAG>%u", self.p2];
+}
+
+@end
+
+@interface YKFOpenPGPBERTLVTag ()
+
+@property(nonatomic, readwrite) NSData *encodedBytes;
+
+@end
+
+@implementation YKFOpenPGPBERTLVTag
+
+typedef struct {
+  UInt64 tagNumber;
+  UInt64 consumedByteCount;
+} TagNumberResult;
+
++ (TagNumberResult)tagNumberFromEncodedBytes:(NSData *)encodedBytes {
+  UInt8 const *bytes = encodedBytes.bytes;
+
+  // Check for single byte tag
+  if (encodedBytes.length == 1 && (bytes[0] & 0x1F) < 0x1F) {
+    TagNumberResult result = {bytes[0] & 0x1F, 1};
+    return result;
+  }
+
+  // Check for multi-byte tag
+  TagNumberResult result = {0, 0};
+
+  for (int i = 0; i < encodedBytes.length; ++i) {
+    result.tagNumber = (result.tagNumber << 7) | (bytes[i] & 0x7F);
+    if ((bytes[i] & 0x80) == 0) {
+      result.consumedByteCount = i + 1;
+      return result;
+    }
+  }
+
+  // Reached the end of the bytes without an end byte of the length
+  result.tagNumber = 0;
+  result.consumedByteCount = 0;
+
+  return result;
+}
+
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes {
+  self = [super init];
+  if (self == nil) {
+    return nil;
+  }
+
+  if (encodedBytes.length == 0) {
+    return nil;
+  }
+
+  UInt8 const *bytes = encodedBytes.bytes;
+
+  // Check for single byte tag
+  if ((bytes[0] & 0x1F) < 0x1F) {
+    self.encodedBytes = [encodedBytes subdataWithRange:NSMakeRange(0, 1)];
+    return self;
+  }
+
+  // Try parsing a tag number from the encoded bytes
+  TagNumberResult parsedTagNumber =
+      [YKFOpenPGPBERTLVTag tagNumberFromEncodedBytes:encodedBytes];
+
+  if (parsedTagNumber.tagNumber == 0) {
+    return nil;
+  }
+
+  self.encodedBytes = [encodedBytes
+      subdataWithRange:NSMakeRange(0, parsedTagNumber.consumedByteCount)];
+
+  return self;
+}
+
+- (instancetype)initWithClass:(YKFOpenPGPBERTLVTagClass)class
+                         type:(YKFOpenPGPBERTLVTagType)type
+                    tagNumber:(UInt64)tagNumber {
+
+  self = [super init];
+  if (self == nil) {
+    return nil;
+  }
+
+  UInt8 leadingByte = (class << 6) | (type << 5);
+
+  // tagNumbers 1-30 encode as a single byte
+  if (tagNumber < 0x1F) {
+    leadingByte |= tagNumber;
+    self.encodedBytes = [NSData dataWithBytes:&leadingByte length:1];
+    return self;
+  }
+
+  // tagNumbers > 30 encode with the leading byte B5-B1 set to 1 and the tag
+  // number encoded in the subsequent bytes. The last byte has the most
+  // significant bit set to 0.
+  NSMutableData *encodedBytes = [[NSMutableData alloc] init];
+  leadingByte |= 0x1F;
+  [encodedBytes appendBytes:&leadingByte length:1];
+
+  // The tag number is encoded in big-endian order.
+  UInt32 subsequentByteCount = tagNumber / 0x7F;
+
+  while ((tagNumber >> (subsequentByteCount * 7)) > 0) {
+    UInt8 subsequentByte =
+        0x80 | (tagNumber >> (subsequentByteCount * 7)) & 0x7F;
+    [encodedBytes appendBytes:&subsequentByte length:1];
+    --subsequentByteCount;
+  }
+
+  // The last byte has the most significant bit set to 0.
+  ((UInt8 *)encodedBytes.mutableBytes)[encodedBytes.length - 1] &= 0x7F;
+
+  self.encodedBytes = encodedBytes;
+
+  return self;
+}
+
+- (UInt64)tagNumber {
+  return [YKFOpenPGPBERTLVTag tagNumberFromEncodedBytes:self.encodedBytes]
+      .tagNumber;
+}
+
+- (UInt8)p1 {
+  UInt16 tag;
+  [self.encodedBytes getBytes:&tag length:sizeof(tag)];
+  return (UInt8)((tag >> 8) & 0xFF);
+}
+
+- (UInt8)p2 {
+  UInt16 tag;
+  [self.encodedBytes getBytes:&tag length:sizeof(tag)];
+  return (UInt8)(tag & 0xFF);
+}
+
+- (void)getParametersP1:(UInt8 *)p1 P2:(UInt8 *)p2 {
+  if (p1 == nil && p2 == nil) {
+    return;
+  }
+
+  *p1 = self.p1;
+  *p2 = self.p2;
+}
+
+@end
+
+@interface YKFOpenPGPSimpleTLVLength ()
+
+@property(nonatomic, readwrite) NSData *encodedBytes;
+
+@end
+
+@implementation YKFOpenPGPSimpleTLVLength
+
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes {
+  self = [super init];
+  if (self == nil) {
+    return nil;
+  }
+
+  if (encodedBytes.length > 3) {
+    return nil;
+  }
+
+  UInt8 const *bytes = encodedBytes.bytes;
+
+  // Check for 1-byte length
+  if (bytes[0] < 0xFF) {
+    return nil;
+  }
+
+  // Check for 3-byte length
+  if (encodedBytes.length < 3) {
+    return nil;
+  }
+
+  // 3-byte length; 1st byte is FF, 2nd and 3rd bytes are the length
+  self.encodedBytes = [encodedBytes subdataWithRange:NSMakeRange(0, 3)];
+
+  return self;
+}
+
+- (instancetype)initWithInteger:(uint16_t)i {
+  self = [super init];
+  if (self) {
+    if (i < 0x80) {
+      UInt8 i2 = i;
+      self.encodedBytes = [NSData dataWithBytes:&i2 length:1];
+    } else if (i <= 0xff) {
+      self.encodedBytes = [NSData dataWithBytes:(UInt8[]){0x81, i} length:2];
+    } else {
+      NSMutableData *data = [NSMutableData alloc];
+      [data appendBytes:(UInt8[]){0x82} length:1];
+
+      uint16_t i2 = htons(i);
+      [data appendBytes:&i2 length:sizeof(i2)];
+
+      self.encodedBytes = data;
+    }
+  }
+  return self;
+}
+
+- (uint16_t)integerValue {
+  uint16_t iv;
+  [self.encodedBytes getBytes:&iv length:sizeof(iv)];
+  return ntohs(iv);
+}
+
+@end
+
+@interface YKFOpenPGPBERTLVLength ()
+
+@property(nonatomic, readwrite) NSData *encodedBytes;
+
+@end
+
+@implementation YKFOpenPGPBERTLVLength
+
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes {
+  self = [super init];
+  if (self == nil) {
+    return nil;
+  }
+
+  if (encodedBytes.length == 0) {
+    return nil;
+  }
+
+  UInt8 const *bytes = encodedBytes.bytes;
+
+  // Check for short form length
+  if (encodedBytes.length == 1 && bytes[0] < 0x80) {
+    self.encodedBytes = [encodedBytes subdataWithRange:NSMakeRange(0, 1)];
+    return self;
+  }
+
+  // Check for long form length
+  UInt8 lengthLength = bytes[0] & 0x7F;
+  if (lengthLength > encodedBytes.length - 1) {
+    return nil;
+  }
+
+  self.encodedBytes =
+      [encodedBytes subdataWithRange:NSMakeRange(0, lengthLength)];
+
+  return self;
+}
+
+- (instancetype)initWithInteger:(UInt16)lengthValue
+                           form:(YKFOpenPGPBERTLVLengthForm)form {
+  self = [super init];
+  if (self == nil) {
+    return nil;
+  }
+
+  if (form == YKFOpenPGPBERTLVLengthFormShort) {
+    if (lengthValue >= 0x80) {
+      return nil;
+    }
+
+    self.encodedBytes = [NSData dataWithBytes:&lengthValue length:1];
+
+  } else if (form == YKFOpenPGPBERTLVLengthFormLong) {
+    if (lengthValue < 0x80) {
+      return nil;
+    }
+
+    if (lengthValue <= 0xFF) {
+      self.encodedBytes = [NSData dataWithBytes:(UInt8[]){0x81, lengthValue}
+                                         length:2];
+    } else if (lengthValue <= 0xFFFF) {
+      uint16_t lengthValueBigEndian = htons(lengthValue);
+      self.encodedBytes =
+          [NSData dataWithBytes:(UInt8[]){0x82, lengthValueBigEndian} length:3];
+    } else {
+      return nil;
+    }
+  }
+
+  return self;
+}
+
+- (uint16_t)integerValue {
+  UInt8 const *bytes = self.encodedBytes.bytes;
+
+  // 1-byte length
+  if (bytes[0] < 0x80) {
+    return bytes[0];
+  }
+
+  // Multi-byte length
+  UInt8 lengthLength = bytes[0] & 0x7F;
+
+  // Null
+  if (lengthLength == 0) {
+    return 0;
+  }
+
+  uint16_t iv = 0;
+  for (int i = 1; i <= lengthLength; ++i) {
+    iv = (iv << 8) | bytes[i];
+  }
+
+  return iv;
+}
+
+@end
+
+@interface YKFOpenPGPSimpleTLV ()
+
+/**
+ * Allow `encodedBytes` to be set internally.
+ */
+@property(nonatomic, readwrite) NSData *encodedBytes;
+@property(nonatomic, readwrite) YKFOpenPGPSimpleTLVTag *tag;
+@property(nonatomic, readwrite) YKFOpenPGPSimpleTLVLength *length;
+@property(nonatomic, readwrite) NSData *value;
+
+@end
+
+@implementation YKFOpenPGPSimpleTLV
+
+- (instancetype)initWithTag:(YKFOpenPGPSimpleTLVTag *)tag
+                     length:(YKFOpenPGPSimpleTLVLength *)length
+                      value:(NSData *)value {
+  self = [super init];
+  if (self == nil) {
+    return nil;
+  }
+
+  self.tag = tag;
+  self.length = length;
+  self.value = value;
+
+  NSMutableData *encodedBytes = [[NSMutableData alloc] init];
+  [encodedBytes appendData:self.tag.encodedBytes];
+  [encodedBytes appendData:self.length.encodedBytes];
+  [encodedBytes appendData:self.value];
+
+  return self;
+}
+
+- (instancetype)initWithEncodedBytes:(NSData *)encodedBytes {
+  self = [super init];
+  if (self == nil) {
+    return nil;
+  }
+
+  // Parse the tag
+  YKFOpenPGPSimpleTLVTag *tag =
+      [[YKFOpenPGPSimpleTLVTag alloc] initWithEncodedBytes:encodedBytes];
+  if (tag == nil) {
+    return nil;
+  }
+  self.tag = tag;
+
+  // Parse the length
+  YKFOpenPGPSimpleTLVLength *length = [[YKFOpenPGPSimpleTLVLength alloc]
+      initWithEncodedBytes:[encodedBytes
+                               subdataWithRange:NSMakeRange(
+                                                    tag.encodedBytes.length,
+                                                    encodedBytes.length)]];
+
+  return self;
+}
+
+@end
+
+#if 0
+
+
+
+@implementation YKFOpenPGPBERTLV
+
+- (NSData *)encodedBytes {
+  NSMutableData *encodedBytes = [[NSMutableData alloc] init];
+  [encodedBytes appendData:self.tag.encodedBytes];
+  [encodedBytes appendData:self.length.encodedBytes];
+  [encodedBytes appendData:self.value];
+  return encodedBytes;
+}
+
+@end
+
+#endif
