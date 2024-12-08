8f2acc5b95841d4312bfa8a420c20cecaafaeead
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2024-12-08 04:41:42 +0000

    YKFOpenPGPError: move error into separate class

---
+++ YubiKit/YubiKit/Connections/Shared/Errors/YKFOpenPGPError.h
@@ -0,0 +1,64 @@
+#import <Foundation/Foundation.h>
+
+/**
+ * @enum YKFOpenPGPErrorDomain
+ *
+ * @brief Error domains for OpenPGP are based on the ISO7816-4 status CLA, INS,
+ * P1, and P2.
+ */
+typedef NS_ENUM(NSUInteger, YKFOpenPGPErrorDomain) {
+  YKFOpenPGPErrorDomainSelectApplication = 0x00A44000,
+  YKFOpenPGPErrorDomainApplicationRelatedData = 0x00CA006E,
+  YKFOpenPGPErrorDomainVerifyPW1CDS = 0x00200081,
+  YKFOpenPGPErrorDomainVerifyPW1Other = 0x00200082,
+  YKFOpenPGPErrorDomainVerifyPW3 = 0x00200083,
+  YKFOpenPGPErrorDomainImplementationSpecific = 0xfffffffff
+};
+
+/**
+ * @enum YKFOpenPGPErrorCode
+ *
+ * @brief Error codes for OpenPGP are based on the ISO7816-4 status words, and
+ * incorporates other from the implementation.
+ *
+ * @details Codes where bytes 1 and 2 are 0x0000 are the ISO7816-4 status words.
+ * Codes where bytes 1 and 2 are non-zero are implementation specific.
+ */
+typedef NS_ENUM(NSUInteger, YKFOpenPGPErrorCode) {
+  YKFOpenPGPErrorCodeSelectedFileInvalidated = 0x6283,
+  YKFOpenPGPErrorCodeInvalidFCI = 0x6284,
+  YKFOpenPGPErrorCodeNotVerified = 0x6300,
+  YKFOpenPGPErrorCodeSecurityStatusNotSatisfied = 0x6982,
+  YKFOpenPGPErrorCodeAuthenticationMethodBlocked = 0x6983,
+  YKFOpenPGPErrorCodeInvalidData = 0x6a80,
+  YKFOpenPGPErrorCodeFunctionNotSupported = 0x6a81,
+  YKFOpenPGPErrorCodeFileNotFound = 0x6a82,
+  YKFOpenPGPErrorCodeIncorrectParameters = 0x6a86,
+  YKFOpenPGPErrorCodeInvalidLc = 0x6a87,
+  YKFOpenPGPErrorCodeReferencedDataNotFound = 0x6a88,
+  YKFOpenPGPErrorCodeWrongParametersP1P2 = 0x6b00,
+  YKFOpenPGPErrorCodeInvalidInstruction = 0x6d00,
+
+  YKFOpenPGPErrorCodeSessionCreation = 0x00010000,
+  YKFOpenPGPErrorCodeSmartCardInterface = 0x00020000,
+  YKFOpenPGPErrorCodeInvalidAID = 0x00030000,
+  YKFOpenPGPErrorCodeInvalidSerialNumber = 0x00040000,
+  YKFOpenPGPErrorCodeInvalidVersion = 0x00050000,
+  YKFOpenPGPErrorCodeInvalidPinFormat = 0x00060000,
+  YKFOpenPGPErrorCodeInvalidMaxChallengeLength = 0x00070000,
+  YKFOpenPGPErrorCodeUnknownError = 0xffff0000
+};
+
+@interface YKFOpenPGPError : NSError
+
+- (instancetype)init NS_UNAVAILABLE;
+
+- (instancetype)initWithDomain:(YKFOpenPGPErrorDomain)domain
+                          code:(YKFOpenPGPErrorCode)code;
+
++ (instancetype)errorWithDomain:(YKFOpenPGPErrorDomain)domain
+                           code:(YKFOpenPGPErrorCode)code;
+
++ (void)buildErrorMap;
+
+@end
---
+++ YubiKit/YubiKit/Connections/Shared/Errors/YKFOpenPGPError.m
@@ -0,0 +1,89 @@
+#import "YKFOpenPGPError.h"
+
+#import <Foundation/Foundation.h>
+
+@implementation YKFOpenPGPError
+
+static NSDictionary<NSNumber *, NSString *> *errorDomain;
+static NSDictionary<NSNumber *, NSString *> *errorCode;
+
+- (instancetype)initWithDomain:(YKFOpenPGPErrorDomain)domain
+                          code:(YKFOpenPGPErrorCode)code {
+  NSString *domainDescription = errorDomain[@(domain)];
+  if (domainDescription == nil) {
+    domainDescription = @"com.yubico.ykf.openpgp";
+  }
+
+  NSString *errorDescription = errorCode[@(code)];
+  if (errorDescription == nil) {
+    errorDescription = @"An unknown error occurred";
+  }
+
+  return [super initWithDomain:domainDescription
+                          code:code
+                      userInfo:@{NSLocalizedDescriptionKey : errorDescription}];
+}
+
+static dispatch_once_t buildErrorMapOnceToken;
+
++ (instancetype)errorWithDomain:(YKFOpenPGPErrorDomain)domain
+                           code:(YKFOpenPGPErrorCode)code {
+
+  dispatch_once(&buildErrorMapOnceToken, ^{
+    [YKFOpenPGPError buildErrorMap];
+  });
+
+  return [[YKFOpenPGPError alloc] initWithDomain:domain code:code];
+}
+
++ (void)buildErrorMap {
+  errorDomain = @{
+    @(YKFOpenPGPErrorDomainSelectApplication) :
+        @"com.yubico.ykf.openpgp-session.select-application",
+    @(YKFOpenPGPErrorDomainApplicationRelatedData) :
+        @"com.yubico.ykf.openpgp-session.application-related-data",
+    @(YKFOpenPGPErrorDomainVerifyPW1CDS) :
+        @"com.yubico.ykf.openpgp-session.verify.pw1.cds",
+    @(YKFOpenPGPErrorDomainVerifyPW1Other) :
+        @"com.yubico.ykf.openpgp-session.verify.pw1.other",
+    @(YKFOpenPGPErrorDomainVerifyPW3) :
+        @"com.yubico.ykf.openpgp-session.verify.pw3",
+    @(YKFOpenPGPErrorDomainImplementationSpecific) :
+        @"com.yubico.ykf.openpgp-session.implementation-specific"
+  };
+
+  errorCode = @{
+    @(YKFOpenPGPErrorCodeSelectedFileInvalidated) : @"Selected file invalidated",
+    @(YKFOpenPGPErrorCodeInvalidFCI) :
+        @"FCI not formatted according to ISO 7816-4",
+    @(YKFOpenPGPErrorCodeNotVerified) : @"Not verified",
+    @(YKFOpenPGPErrorCodeSecurityStatusNotSatisfied) :
+        @"Security status not satisfied/PW wrong/PW not checked (command not "
+        @"allowed)",
+    @(YKFOpenPGPErrorCodeAuthenticationMethodBlocked) :
+        @"Authentication method blocked/PW blocked (error counter zero)",
+    @(YKFOpenPGPErrorCodeInvalidData) :
+        @"Incorrect parameter in command data field",
+    @(YKFOpenPGPErrorCodeFunctionNotSupported) : @"Function not supported",
+    @(YKFOpenPGPErrorCodeFileNotFound) : @"File not found",
+    @(YKFOpenPGPErrorCodeIncorrectParameters) : @"Incorrect parameters P1-P2",
+    @(YKFOpenPGPErrorCodeInvalidLc) : @"Lc inconsistent with P1-P2",
+    @(YKFOpenPGPErrorCodeReferencedDataNotFound) : @"Referenced data not found",
+    @(YKFOpenPGPErrorCodeWrongParametersP1P2) : @"Incorrect parameters P1-P2",
+    @(YKFOpenPGPErrorCodeInvalidInstruction) :
+        @"Instruction not supported or invalid",
+
+    @(YKFOpenPGPErrorCodeSessionCreation) : @"Failed to create OpenPGP session",
+    @(YKFOpenPGPErrorCodeSmartCardInterface) :
+        @"Failed to create SmartCardInterface",
+    @(YKFOpenPGPErrorCodeInvalidAID) : @"Failed parsing AID",
+    @(YKFOpenPGPErrorCodeInvalidSerialNumber) : @"Failed parsing serial number",
+    @(YKFOpenPGPErrorCodeInvalidVersion) : @"Failed parsing version",
+    @(YKFOpenPGPErrorCodeInvalidPinFormat) : @"Failed parsing PIN format",
+    @(YKFOpenPGPErrorCodeInvalidMaxChallengeLength) :
+        @"Failed parsing maximum challenge length",
+    @(YKFOpenPGPErrorCodeUnknownError) : @"An unknown error occurred",
+  };
+}
+
+@end
